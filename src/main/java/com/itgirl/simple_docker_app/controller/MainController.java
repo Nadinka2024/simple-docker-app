66d9d3687fe04e8a1f8ba3db8c45fdf83b5a1490
nadezhdachurilo <nadezhdachurilo@gmail.com>
2025-04-15 21:40:30 +0200

    Initial commit

---
+++ src/main/java/com/itgirl/simple_docker_app/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.itgirl.simple_docker_app.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("index")
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
